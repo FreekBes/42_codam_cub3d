c870d451a3d021ed16a70248b8926673139b0b4a
Freek Bes <freekbladnl@gmail.com>
2021-05-13 21:19:03 +0200

    norminette-proofing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fbes <fbes@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/24 16:40:50 by fbes          #+#    #+#                 */
-/*   Updated: 2021/05/13 20:11:04 by fbes          ########   odam.nl         */
+/*   Updated: 2021/05/13 21:18:51 by fbes          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,14 +34,44 @@ static void	init_game_win(t_game *game)
 {
 	reset_key_presses(&game->key_stat);
 	mlx_mouse_hide(OS_MLX_REQ_PARAMS_MOUSE_SHOW_HIDE);
-	mlx_mouse_move(OS_MLX_REQ_PARAMS, game->map->res_x / 2, game->map->res_y / 2);
-	mlx_hook(game->mlx->win, 17, 1L<<17, &exit_hook, game);
-	mlx_hook(game->mlx->win, 2, 1L<<0, &keypress, game);
-	mlx_hook(game->mlx->win, 3, 1L<<1, &keyrelease, game);
-	mlx_hook(game->mlx->win, 4, 1L<<2, &mousebtnpress, game);
-	mlx_hook(game->mlx->win, 6, 1L<<6, &mousemove, game);
-	mlx_hook(game->mlx->win, 9, 1L<<21, &win_focus, game);
+	mlx_mouse_move(OS_MLX_REQ_PARAMS,
+		game->map->res_x / 2, game->map->res_y / 2);
+	mlx_hook(game->mlx->win, 17, 1L << 17, &exit_hook, game);
+	mlx_hook(game->mlx->win, 2, 1L << 0, &keypress, game);
+	mlx_hook(game->mlx->win, 3, 1L << 1, &keyrelease, game);
+	mlx_hook(game->mlx->win, 4, 1L << 2, &mousebtnpress, game);
+	mlx_hook(game->mlx->win, 6, 1L << 6, &mousemove, game);
+	mlx_hook(game->mlx->win, 9, 1L << 21, &win_focus, game);
 	mlx_loop_hook(game->mlx->core, draw_next_frame, game);
+}
+
+static int	setup_map(t_game *game, int save_bmp, char **argv)
+{
+	game->cam.speed_mod = 1;
+	game->cam.mouse_sens = CAM_DEFAULT_MOUSE_SENSITIVITY;
+	game->cam.z_buffer = NULL;
+	game->map = parse_map(argv[1]);
+	if (!game->map)
+		return (print_error("Failed to read or parse map"));
+	if (set_starting_pos(game) < 0)
+		return (print_error("Start position in map is not set"));
+	printf("start pos: %f, %f\n", game->cam.pos_x, game->cam.pos_y);
+	if (!map_surrounded_by_walls(game))
+		return (print_error("Map is not surrounded by walls"));
+	if (save_bmp == 0)
+		game->mlx = get_mlx_context(game->map, argv[0]);
+	else
+		game->mlx = get_mlx_context(game->map, NULL);
+	if (!game->mlx)
+		exit_game(*game, "Failed to create MLX instance");
+	if (parse_textures(game) < 0)
+		exit_game(*game, "Failed to parse textures, do the files exist?");
+	parse_sprites(game);
+	game->cam.z_buffer = (double *)malloc(sizeof(double) * game->map->res_x);
+	if (!game->cam.z_buffer)
+		exit_game(*game, "Could not allocate memory for z_buffer");
+	print_map(*(game->map), NULL);
+	return (1);
 }
 
 int	main(int argc, char **argv)
@@ -54,30 +84,8 @@ int	main(int argc, char **argv)
 		save_bmp = 1;
 	if (argc < 2)
 		return (print_error("No map specified as first argument"));
-	game.cam.speed_mod = 1;
-	game.cam.mouse_sens = CAM_DEFAULT_MOUSE_SENSITIVITY;
-	game.cam.z_buffer = NULL;
-	game.map = parse_map(argv[1]);
-	if (!game.map)
-		return (print_error("Failed to read or parse map"));
-	if (set_starting_pos(&game) < 0)
-		return (print_error("Start position in map is not set"));
-	printf("start pos: %f, %f\n", game.cam.pos_x, game.cam.pos_y);
-	if (!map_surrounded_by_walls(&game))
-		return (print_error("Map is not surrounded by walls"));
-	if (save_bmp == 0)
-		game.mlx = get_mlx_context(game.map, argv[0]);
-	else
-		game.mlx = get_mlx_context(game.map, NULL);
-	if (!game.mlx)
-		exit_game(game, "Failed to create MLX instance");
-	if (parse_textures(&game) < 0)
-		exit_game(game, "Failed to parse textures, do all texture files exist?");
-	parse_sprites(&game);
-	game.cam.z_buffer = (double *)malloc(sizeof(double) * game.map->res_x);
-	if (!game.cam.z_buffer)
-		exit_game(game, "Could not allocate memory for z_buffer");
-	print_map(*(game.map), NULL);
+	if (setup_map(&game, save_bmp, argv) == 0)
+		return (0);
 	if (save_bmp == 0)
 	{
 		init_game_win(&game);
